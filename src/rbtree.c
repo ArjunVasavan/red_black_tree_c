9ddd810623e0379ee7e6519beec45dcafd347940
Arjun Vasavan <vasavanarjun@gmail.com>
2026-03-19 22:01:10 +0530

    feat: added fix insert function

--- src/rbtree.c
+++ src/rbtree.c
@@ -18,7 +18,75 @@ node* create_node(int value) {
     return new_node;
 }
 
+void fix_insert(node** root, node* new_node ) {
 
+    while ( new_node != *root && new_node->parent_pointer->colour == RED ) {
+
+        node* parent_node = new_node->parent_pointer;
+        node* grand_node =  parent_node->parent_pointer;
+
+        if ( parent_node == grand_node->left_pointer ) { // bented to left side
+
+            node* uncle_node = grand_node->right_pointer;
+
+            if ( uncle_node != NULL && uncle_node->colour == RED ) {
+
+                parent_node->colour = BLACK;
+                grand_node->colour = RED;
+                uncle_node->colour = BLACK;
+
+                new_node = grand_node;
+
+            } else {
+
+                if ( new_node == parent_node->right_pointer ) { // now its on LR fashion
+
+                    left_rotate(root,parent_node);
+
+
+                    new_node = parent_node;
+                    parent_node = new_node->parent_pointer;
+
+                }
+                // case for RR or left over of LRs R rotation
+
+                grand_node->colour = RED;
+                parent_node->colour = BLACK;
+
+                right_rotate(root,grand_node);
+            }
+        } else { // bented to right side
+
+            node* uncle_node = grand_node->right_pointer;
+
+            if ( uncle_node != NULL && uncle_node->colour == RED ) {
+
+                parent_node->colour = BLACK;
+                uncle_node->colour = BLACK;
+                grand_node->colour = RED;
+
+                new_node = grand_node;
+                
+            } else {
+
+                if ( parent_node == grand_node->left_pointer ) { // RL case
+
+                    right_rotate(root,parent_node);
+                    new_node = parent_node;
+                    parent_node = new_node->parent_pointer;
+
+                }
+
+                grand_node->colour = RED;
+                parent_node->colour = BLACK;
+
+                left_rotate(root,grand_node);
+            }
+        }
+    }
+
+    (*root)->colour = BLACK;
+}
 
 void insert(node **root, int value) {
 
@@ -51,7 +119,7 @@ void insert(node **root, int value) {
         par->right_pointer = new_node;
     }
 
-    printf("[insert]: (%d,%d)\n",new_node->data,new_node->colour);
+    fix_insert(root,new_node);
 }
 
 void search(node** root, int value ) {
